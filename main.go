7d222a1ca6e0d362f6bc88635aeac06a00935ed4
fabiomassucatto <fabiomassucatto0102@gmail.com>
2025-11-14 11:30:15 -0300

    requisição okk

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/fabiomassucatto/gin-go-api/database"
-	"github.com/fabiomassucatto/gin-go-api/models"
 	"github.com/fabiomassucatto/gin-go-api/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Fabio Henrique", CPF: "123.456.789-00", RG: "12.345.678-9"},
-		{Nome: "Maria Silva", CPF: "987.654.321-00", RG: "98.765.432-1"},
-		{Nome: "João Souza", CPF: "456.789.123-00", RG: "45.678.912-3"},
-	}
 	routes.HandleRequests()
 }
